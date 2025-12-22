b3b4d89cd832689fdd9b4b96b5a01efa3ba5bc26
s0um0r0y <soumoroy09@gmail.com>
2025-12-22 18:18:33 +0530

    [FEATURE]: added docker image for deployment (#3)

---
+++ Docker/run_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+xhost +local:docker
+DATA_DIR=${1:-$(pwd)/data}
+
+echo "Starting SALT container..."
+echo "Mapping Data Directory: $DATA_DIR"
+
+docker run -it --rm \
+    --gpus all \
+        --net=host \
+        --env="DISPLAY" \
+        --env="QT_X11_NO_MITSHM=1" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --volume="$DATA_DIR:/app/data" \
+        --volume="$(pwd)/annotations:/app/annotations" \
+        --name salt_app \
+        salt_tool
+
+xhost -local:docker
