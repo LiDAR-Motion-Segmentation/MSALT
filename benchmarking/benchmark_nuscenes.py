f23e5dea6beb0b2e8315894e67ea9d3c44ea9b8c
s0um0r0y <soumoroy09@gmail.com>
2026-02-06 18:20:12 +0530

    [FIX]: updated paths with config (#20)

--- benchmarking/benchmark_nuscenes.py
+++ benchmarking/benchmark_nuscenes.py
@@ -5,13 +5,8 @@ from omegaconf import DictConfig, OmegaConf
 
 from benchmarking.evaluators.nuscenes_evaluator import NuScenesEvaluator
 
-@hydra.main(config_path="../config", config_name="benchmark.yaml", version_base=None)
+@hydra.main(config_path="../config", config_name="benchmark_nuscenes.yaml", version_base=None)
 def main(cfg: DictConfig):
-    
-    # # Validation
-    # if Path(cfg.output_dir).exists():
-    #     logging.error("You must specify output_dir! Example: python benchmark_nuscenes.py output_dir=./results")
-    #     return
 
     # Print config to verify
     print(OmegaConf.to_yaml(cfg))
