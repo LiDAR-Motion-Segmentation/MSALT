d248358fe0312980f4aa0b1c789b3408f159ed50
s0um0r0y <soumoroy09@gmail.com>
2026-02-08 13:55:45 +0530

    [FIX]: playback slider frame update bug (#3)

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -555,6 +555,7 @@ class MainWindow(QMainWindow):
 
         # Jump to the next frame so user can see the result
         self.load_frame(next_idx)
+        self.playback.slider.setValue(next_idx)
         self.save_current_work()
         self.statusBar().showMessage(
             f"Propagated {count} objects to Frame {next_idx}", 2000
